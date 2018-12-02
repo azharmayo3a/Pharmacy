bf02c2a914a24c1864e180bf2c03c901fee2884c
Azharmayo3a <azharmayo3a@gmail.com>
2018-12-02 04:40:34 +0100

    [Add]
    Add user  login

---
+++ Pharmacy.UI/ForgotPassword.aspx
@@ -0,0 +1,34 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="ForgotPassword.aspx.cs" Inherits="ForgotPassword" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Forgot Password</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <table>
+            <tr>
+                <td>
+                    Email:
+                </td>
+                <td>
+                    <asp:TextBox ID="txtEmail" AutoComplete="off" runat="server"></asp:TextBox>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <asp:Button ID="btnSendEmail" runat="server" Text="Send Email" OnClick="btnSendEmail_Click" />
+                </td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <asp:Label ID="lblMessage" runat="server"></asp:Label>
+                </td>
+            </tr>
+        </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Pharmacy.UI/ForgotPassword.aspx.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Pharmacy.DAL;
+using System.Data;
+using System.Net.Mail;
+public partial class ForgotPassword : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+    protected void btnSendEmail_Click(object sender, EventArgs e)
+    {
+        DALUser obj = new DALUser();
+        DataSet ds = obj.User_GetLoginDetails(txtEmail.Text.Trim());
+
+        if (ds.Tables[0].Rows.Count > 0)
+        {
+            string from = "m.zahidkamran@gmail.com";
+            string to = txtEmail.Text.Trim();
+
+            MailMessage mail = new MailMessage();
+            mail.To.Add(to);
+            mail.From = new MailAddress(from, "My Pharmacy", System.Text.Encoding.UTF8);
+            mail.Subject = "Password Recovery Email";
+            mail.SubjectEncoding = System.Text.Encoding.UTF8;
+            mail.Body = "Dear Valued Customer," + ds.Tables[0].Rows[0]["FullName"].ToString() + "<br/>" +
+                        "Your login details are as follows:<br/><br/>" +
+                        "<b>Member ID:" + txtEmail.Text.Trim() + "</b> <br/>" +
+                        "<b>Password:" + ds.Tables[0].Rows[0]["Password"].ToString() + "</b> <br/><br/>" +
+                        "MyPharmacy Support Team<br/>" +
+                        "www.mypharmacy.com";
+            mail.BodyEncoding = System.Text.Encoding.UTF8;
+            mail.IsBodyHtml = true;
+            mail.Priority = MailPriority.High;
+
+            SmtpClient client = new SmtpClient();
+            client.Credentials = new System.Net.NetworkCredential(from, "bsit6861668");
+
+            //client.UseDefaultCredentials = true;
+            client.Port = 25; // Gmail works on this port
+            client.Host = "smtp.gmail.com";
+            client.EnableSsl = true; //Gmail works on Server Secured Layer
+            try
+            {
+                client.Send(mail);
+                lblMessage.Text = "Email sent successfully!";
+            }
+            catch (Exception ex)
+            {
+                obj.ErrorLog_Add(ex.Message);
+                lblMessage.Text = "Our email server is in process of upgradation.\nTry after sometime.";
+            }
+        }
+        else
+        {
+            lblMessage.Text = "Invalid Email";
+        }
+    }
+}
